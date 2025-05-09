9a8f7db86e004002c280f10d5a54749f2099a13b
John McDole <codefu@google.com>
2025-05-09 13:52:05 -0700

    Initial layout

---
+++ server/bin/server.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:namer/namer.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+// Configure routes.
+final _router =
+    Router()
+      ..get('/', _rootHandler)
+      ..get('/api/name', _nameHandler);
+
+Response _rootHandler(Request req) {
+  // todo: static route to flutter web
+  return Response.ok('Hello, World!\n');
+}
+
+Response _nameHandler(Request req) {
+  return Response.ok(json.encode({'name': animal(adjectives: 0, verbs: 1)}));
+}
+
+void main(List<String> args) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addHandler(_router.call);
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
